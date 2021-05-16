de6ea1aaa0feb21bd4ab988a990f9ad970005476
VIDUR MODGIL <vmod2005@gmail.com>
2021-05-16 18:40:36 -0400

    Attempting to add a basic GUI to the game

---
+++ Graphics Test.py
@@ -0,0 +1,27 @@
+'''
+Vidur Modgil
+Oregon Trail Graphics Test
+AP World History
+Mrs. Fitzpatrick
+'''
+
+# Preprossescor Directives
+import turtle
+import os
+
+def prompt(category, prompt):
+    action = turtle.textinput(category, prompt)
+    return action
+
+def main():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'Background Map.png')
+    tr = turtle.Turtle()
+    wn = turtle.Screen()
+    wn.bgpic(filename)
+    tr.penup()
+    tr.goto(x=-350, y=100)
+    wn.mainloop()
+
+if __name__ == '__main__':
+    main()
